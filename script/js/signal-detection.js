331f8c07cb68001b6aebd35104048e6a34a6737a
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2020-02-06 23:23:47 -0500

    initial commit

---
+++ script/js/signal-detection.js
@@ -0,0 +1,29 @@
+window.baseHue = 180;
+
+function updateColors(hueDifference) {
+    document.getElementById("exampleA")
+        .style.backgroundColor = `hsl(${window.baseHue + hueDifference}, 75%, 50%)`;
+    document.getElementById("exampleB")
+        .style.backgroundColor = `hsl(${window.baseHue - hueDifference}, 75%, 50%)`;
+}
+
+function runTrial() {
+    var stimulus = document.getElementById("stimulus");
+    var probSlider = document.getElementById("prob-slider");
+    var choice = Math.random() < probSlider.value / 100 ? "A" : "B";
+    console.log(choice);
+    stimulus.style.backgroundColor = document.getElementById("example" + choice).style.backgroundColor;
+    stimulus.style.opacity = 1;
+    setTimeout(function () {
+        stimulus.style.opacity = 0;
+    }, 50);
+}
+
+window.onload = function () {
+    var noiseSlider = document.getElementById("noise-slider");
+    updateColors((1 - (noiseSlider.value / 100)) * 100);
+    noiseSlider.addEventListener("input", function (e) {
+        updateColors((1 - (e.target.value / 100)) * 100);
+    });
+    document.getElementById("start-button").addEventListener("click", runTrial);
+};
